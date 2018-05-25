093e864aaf975fb5cbec6ac231467bec9f752f67
Albert Abraham <abraham.albert.albert@gmail.com>
2018-05-25 16:37:55 +0530

    Completed profile creating and listing feature

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework import status
 from . import serializers
+from . import models
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -63,3 +64,42 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message':'Hello!', 'a_viewset':a_viewset})
+
+    def create(self, request):
+        """Create a hello message with our name"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by ID."""
+
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object"""
+
+        return Response({'http_method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handles removing an object"""
+
+        return Response({'http_method':'DELETE'})
+
+class UserProfilesViewSet(viewsets.ModelViewSet):
+    """ Handles creating, reading and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
